4497c624ee407c7d77356eb53f478179db44bfc7
Nick <nsagias@gmail.com>
2021-11-15 16:54:36 -0500

    git push error, copied everything from traversal from this branch

--- vampire.js
+++ vampire.js
@@ -10,23 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
 
+    let numberOfVampires = 0;
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
+
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
-  }
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;  }
 
   /** Stretch **/
 
@@ -37,6 +47,31 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+  }
+
+  vampireWithName(name) {
+    // find a vanmpire with a specific name
+    // return name if that vampire does not exist
+    let vampireName = null;
+    
+
+    return vampireName;
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let descendents = 0;
+    
+
+    return descendents;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let millennialVampires = [];
+
+
+    return millennialVampires;
   }
 }
 
